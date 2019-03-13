e123d53ae2de9dab0406ac8938bb51d106c825cf
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-13 19:05:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,24 +7,6 @@ class Shoe
       @brand = brand
     end
 
-    def color=(color)
-      @color = color
-
-    end
-
-    def size=(size)
-      @size = size
-
-    end
-
-    def material=(material)
-      @material = material
-
-    end
-
-    def condition=(condition)
-      @condition = condition
-    end
     def cobble()
       puts "Your shoe is as good as new!"
     end
