998d156c80c2eee01ad04db908164ee53a0f495c
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-13 18:47:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
+  attr_accessor :color, :size, :material, :condition, :cobble
     attr_reader :brand,
-    attr_accessor  :size, :material, :condition, :cobble
 
     def initialized(brand)
       @brand = brand
